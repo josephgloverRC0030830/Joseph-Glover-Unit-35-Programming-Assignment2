adf6dc9a9ada80ab2ccfe30c3293bd2f934445a9
Joseph Glover <rc0030830@CRO-ICF07-02N>
2018-06-06 14:09:05 +0100

    bolox i need

--- Joseph Glover, Unit 35, Programming, Assignment2/Form1.cs
+++ Joseph Glover, Unit 35, Programming, Assignment2/Form1.cs
@@ -7,14 +7,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO;
 namespace Joseph_Glover__Unit_35__Programming__Assignment2
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        class row
+        {
+            public double time;
+            public double charge;
+        }
+
+        List<row> table = new List<row>();
+        
+            public Form1()
         {
             InitializeComponent();
         }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "csv Files|*.csv";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader (openFileDialog1.FileName))
+                    {
+                        string line = sr.ReadLine();
+                        while (!sr.EndOfStream)
+                        {
+                            table.Add(new row());
+                            string[] r = sr.ReadLine().Split(',');
+                            table.Last().time = double.Parse(r[0]);
+                            table.Last().charge = double.Parse(r[1]);
+                        }
+                    }
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + " failed to open.");
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + " is not in the required format.");
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + " failed to open.");
+                }
+            }
     }
 }
